dd97465a1699b31535c9ba3c7bcf6e611e4b24f3
SINEAD KELLY - STUDENT <G00357595@gmit.ie>
2018-02-26 11:51:00 +0000

    Week 21 Lava Lab

---
+++ Week 21-20180226/Continue.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Continue
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+   		int number;
+
+   		System.out.print("Enter a number from 1 to 10: ");
+   		number = input.nextInt();
+
+   		for (int counter = 1; counter <= 10; counter++)
+   		{
+   			if(counter == number)
+   			continue;
+
+   			System.out.println(counter);
+   		}
+   		System.out.println("\nUsed continue to skip printing " +number);
+	}
+}
+
---
+++ Week 21-20180226/Sales.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Sales
+{
+	public static void main(String[] args)
+	{
+ 		Scanner input = new Scanner(System.in);
+ 		int productNumber;
+ 		double total = 0;
+ 		int quantity;
+
+		System.out.println("Enter product number (1-5 or 0 to stop): ");
+		productNumber = input.nextInt();
+
+ 		while (productNumber != 0)
+ 		{
+
+			if(productNumber >= 1 && productNumber <= 5)
+			{
+				System.out.println("Enter quantity sold: ");
+ 				quantity = input.nextInt();
+
+ 				switch(productNumber)
+ 				{
+					case 1:
+						total+= quantity * 2.98;
+						break;
+					case 2:
+						total+= quantity * 4.50;
+						break;
+					case 3:
+						total+= quantity * 9.98;
+						break;
+					case 4:
+						total+= quantity * 4.49;
+						break;
+					case 5:
+						total+= quantity * 6.87;
+						break;
+				}
+			}
+			else if(productNumber != 0)
+				System.out.println("Product number must be between 1 and 5 included");
+
+				System.out.println("Enter product number (1-5 or 0 to stop): ");
+
+		}
+		System.out.println("Total of products sold: ");
+		System.out.println(Math.round(total*10000.0/10000.0));
+	}
+}
