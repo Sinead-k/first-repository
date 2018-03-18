bff5be3b5351b3bbb03483e8c6b1088e222cf920
Sinéad Kelly <g00357595@gmit.ie>
2018-03-18 14:34:53 +0000

    15/03/18

---
+++ Index/FirstArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FirstArray
+{	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int [] x;
+		x = new int[4];
+
+		for(int counter = 0; counter < x.length; counter++)
+		{
+			System.out.println(counter);
+		}
+		Index.main(null);
+	}
+}
---
+++ Index/SecondArray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SecondArray
+{	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int [] x = {128, 132, 8, 156, 18};
+
+		for(int counter = 0; counter < x.length; counter++)
+		{
+			System.out.println(x[counter]);
+		}
+		Index.main(null);
+	}
+}
